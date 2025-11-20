f98f2f79508de93e42c09b2081cbd6cfc95be9cc
Chircami <chircami@gmail.com>
2025-11-20 21:46:34 +0100

    Set site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://chircami.github.io',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
